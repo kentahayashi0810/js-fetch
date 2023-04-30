8aa8e6fe3da21a0084342d174a46f4c45f7b6f97
kentahayashi <kenta0810hayashi@gmail.com>
2023-04-30 23:37:02 +0200

    added price

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const fetchButton = document.getElementById("js-fetchButton");
+const fetchInput = document.getElementById("js-fetchInput");
+const fetchTitle = document.getElementById("js-fetchTitle");
+const fetchPrice = document.getElementById("js-fetchPrice");
 
 fetchButton.addEventListener("click", () => {
-  fetch("data.json")
-    .then((res) => res.json())
-    .then((data) => console.log(data.books[1].title));
+  const bookNum = parseInt(fetchInput.value);
+  if (typeof bookNum == "number") {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.books[bookNum].title);
+        fetchTitle.innerHTML = data.books[bookNum].title;
+        fetchPrice.innerHTML = data.books[bookNum].price;
+      })
+      .catch(() => {
+        console.log("We seemed to have failed to get the data.");
+      });
+  } else {
+    alert("The input must be a number");
+  }
 });
