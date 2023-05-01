6235a75d28899a1379a79f5ab2ff8e6232c39bda
kentahayashi <kenta0810hayashi@gmail.com>
2023-05-01 17:14:35 +0200

    Added search feature by publisher name

--- app.js
+++ app.js
@@ -11,12 +11,38 @@ fetchButton.addEventListener("click", () => {
       .then((data) => {
         console.log(data.books[bookNum].title);
         fetchTitle.innerHTML = data.books[bookNum].title;
-        fetchPrice.innerHTML = data.books[bookNum].price;
+        fetchPrice.innerHTML = data.books[bookNum].price + "円";
       })
       .catch(() => {
-        console.log("We seemed to have failed to get the data.");
+        console.log("We seem to have failed to get the data.");
       });
   } else {
     alert("The input must be a number");
   }
+});
+
+const fetchButton2 = document.getElementById("js-fetchButton2");
+const fetchInput2 = document.getElementById("js-fetchInput2");
+const fetchTitle2 = document.getElementById("js-fetchTitle2");
+const fetchPrice2 = document.getElementById("js-fetchPrice2");
+
+fetchButton2.addEventListener("click", () => {
+  const bookPublisher = fetchInput2.value;
+
+  fetch("data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data["books"]);
+      const books = data["books"];
+      for (const item of books) {
+        if (item["publisher"] == bookPublisher) {
+          fetchTitle2.innerHTML = item.title;
+          fetchPrice2.innerHTML = item.price + "円";
+        }
+      }
+    })
+    .catch(() => {
+      console.log("We seem to have failed to get the data.");
+    });
 });
